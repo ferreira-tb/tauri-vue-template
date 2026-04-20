27ae03cb0a6b6a820b47bc88ac03423fc9e535bf
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-19 22:16:03 -0300

    chore: generate bindings

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@tb-dev/eslint-config';
 
 export default defineConfig({
   project: ['tsconfig.json', 'app/tsconfig.json'],
-  ignores: [],
+  ignores: ['**/bindings.ts'],
   features: {
     vue: true,
   },
